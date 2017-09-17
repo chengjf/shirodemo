870578db578ed29524978dd09059c1a81cca3863
Jeff Cheng <jeff@JeffdeMBP.lan>
2017-09-17 13:23:29 +0800

    Revert "Revert "Initialization repo.""
    This reverts commit 5d3cb62858fcc152236064f92652f1ae03f56399.

---
+++ src/main/java/com/chengjf/shiro/demo/controllers/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.chengjf.shiro.demo.controllers;
+
+public class NotFoundException extends Exception {
+
+    public NotFoundException(String id) {
+        super(id);
+    }
+}
---
+++ src/test/java/com/chengjf/shiro/demo/LoginTest.java
@@ -0,0 +1,39 @@
+package com.chengjf.shiro.demo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {SpringBootApp.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LoginTest {
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void test() throws JsonProcessingException {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+        map.add("username", "chengjf");
+        map.add("password", "chengjf");
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+        ResponseEntity<String> stringResponseEntity = testRestTemplate.postForEntity("/login", request, String.class);
+        System.out.println(stringResponseEntity.getBody());
+
+    }
+}
