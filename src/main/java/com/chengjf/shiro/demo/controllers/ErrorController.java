5d3cb62858fcc152236064f92652f1ae03f56399
Jeff Cheng <jeff@JeffdeMBP.lan>
2017-09-17 13:19:18 +0800

    Revert "Initialization repo."
    This reverts commit f29f5cdeb97910c19f8e16750bd8b07d069e2045.

--- src/main/java/com/chengjf/shiro/demo/controllers/ErrorController.java
+++
@@ -1,9 +0,0 @@
-package com.chengjf.shiro.demo.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ErrorController {
-
-
-}
--- src/main/java/com/chengjf/shiro/demo/controllers/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.chengjf.shiro.demo.controllers;
-
-public class NotFoundException extends Exception {
-
-    public NotFoundException(String id) {
-        super(id);
-    }
-}
